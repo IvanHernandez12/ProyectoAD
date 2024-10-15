1f26ffa83585c229645b113d01714eb795840902
User <User@DESKTOP-KN9CNNP>
2024-10-15 15:25:02 -0600

    detalleventa Atributos y constructores

--- src/main/java/Proyecto/DetalleVenta.java
+++ src/main/java/Proyecto/DetalleVenta.java
@@ -9,5 +9,76 @@ package Proyecto;
  * @author User
  */
 public class DetalleVenta {
+    private int cant;
+    private Producto Producto;
+    private long codigoVenta;
+    private float precioUnitario;
+
+    public DetalleVenta() {
+    }
+
+    public DetalleVenta(int cant, Producto Producto, long codigoVenta, float precioUnitario) {
+        this.cant = cant;
+        this.Producto = Producto;
+        this.codigoVenta = codigoVenta;
+        this.precioUnitario = precioUnitario;
+    }
+
+    /**
+     * @return the cant
+     */
+    public int getCant() {
+        return cant;
+    }
+
+    /**
+     * @param cant the cant to set
+     */
+    public void setCant(int cant) {
+        this.cant = cant;
+    }
+
+    /**
+     * @return the Producto
+     */
+    public Producto getProducto() {
+        return Producto;
+    }
+
+    /**
+     * @param Producto the Producto to set
+     */
+    public void setProducto(Producto Producto) {
+        this.Producto = Producto;
+    }
+
+    /**
+     * @return the codigoVenta
+     */
+    public long getCodigoVenta() {
+        return codigoVenta;
+    }
+
+    /**
+     * @param codigoVenta the codigoVenta to set
+     */
+    public void setCodigoVenta(long codigoVenta) {
+        this.codigoVenta = codigoVenta;
+    }
+
+    /**
+     * @return the precioUnitario
+     */
+    public float getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    /**
+     * @param precioUnitario the precioUnitario to set
+     */
+    public void setPrecioUnitario(float precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+    
     
 }
